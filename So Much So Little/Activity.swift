811fbd84790fd144797948ad01fa6592e901630c
Adland Lee <adland@4mfd.com>
2016-06-20 15:31:06 -0700

    Add main menu tab controller

---
+++ So Much So Little/Activity.swift
@@ -0,0 +1,43 @@
+//
+//  Activity.swift
+//  So Much So Little
+//
+//  Created by Adland Lee on 6/16/16.
+//  Copyright © 2016 Adland Lee. All rights reserved.
+//
+
+import CoreData
+import Foundation
+
+class Activity: NSManagedObject {
+    
+    struct Keys {
+        static let Task = "task"
+        static let Project = "project"
+        static let Complete = "complete"
+        static let EstimatedTimeboxes = "estimated_timeboxes"
+        static let ActualTimeboxes = "actual_timeboxes"
+        static let DeferredTo = "deferred_to"
+        static let DeferredToResponseDue = "deferred_to_response_due"
+        static let ScheduledStart = "scheduled_start"
+        static let ScheduledAdjourn = "scheduled_adjourn"
+        static let Role = "role"
+        static let DueDate = "due_date"
+        static let InterruptionCount = "interruptions_count"
+    }
+    
+    static let mockActivityList: [[String:AnyObject]] = [
+        [
+            Keys.Task: "Activity Alpha",
+            Keys.EstimatedTimeboxes: 4
+        ],
+        [
+            Keys.Task: "Activity Bravo",
+            Keys.EstimatedTimeboxes: 2
+        ],
+        [
+            Keys.Task: "Activity Charlie",
+            Keys.EstimatedTimeboxes: 2
+        ]
+    ]
+}
--- So Much So Little/TodayViewController.swift
+++ So Much So Little/TodayViewController.swift
@@ -8,10 +8,71 @@
 
 import UIKit
 
+class TodayTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var timeBoxTallyLabel: UILabel!
+    @IBOutlet weak var taskLabel: UILabel!
+}
+
 class TodayViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var activities:[[String:AnyObject]] {
+        return Activity.mockActivityList
+    }
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
+
+// MARK: - Table View Data Source
+
+extension TodayViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return activities.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let identifier = "TodayCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier) as! TodayTableViewCell
+        
+        configureTodayCell(cell, atIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    func configureTodayCell(cell: TodayTableViewCell, atIndexPath indexPath: NSIndexPath) {
+        let activity = activities[indexPath.row]
+        let actualTimeboxes = activity[Activity.Keys.ActualTimeboxes] as? Int ?? 0
+        let estimatedTimeboxes = activity[Activity.Keys.EstimatedTimeboxes] as? Int ?? 0
+        
+        cell.taskLabel.text = activity[Activity.Keys.Task] as? String
+        cell.timeBoxTallyLabel.text = "\(actualTimeboxes)/\(estimatedTimeboxes)"
+    }
+}
+
+
+// MARK: - Table View Delegate
+
+extension TodayViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+        let unlist = UITableViewRowAction(style: .Normal, title: "Unlist") { (action, todayIndexPath) in
+            print("\(todayIndexPath.row): Unlist tapped")
+        }
+        
+        let complete = UITableViewRowAction(style: .Normal, title: "Complete") { (action, completeIndexPath) in
+            print("\(completeIndexPath.row): Complete tapped")
+        }
+        
+        return [unlist, complete]
+    }
+}
