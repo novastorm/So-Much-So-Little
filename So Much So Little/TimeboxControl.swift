b41483fb11148ade1536abc94b63097b00bf629f
Adland Lee <adland@4mfd.com>
2016-06-27 13:55:01 -0700

    add visual time box selection stub

--- So Much So Little/TimeboxControl.swift
+++ So Much So Little/TimeboxControl.swift
@@ -9,19 +9,47 @@
 import UIKit
 
 class TimeboxControl: UIView {
+    
+    // MARK: Properties
+    
+    var timeboxButtons = [UIButton]()
+    
+    let timeboxSpacing = 5
+    let timeBoxCount = 7
 
-    // MARK: Initialization
+    // MARK: - Initialization
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.redColor()
+        for _ in 0 ..< timeBoxCount {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: #selector(TimeboxControl.timeboxButtonTapped(_:)), forControlEvents: .TouchDown)
+            
+            timeboxButtons += [button]
+            
+            addSubview(button)
+        }
+    }
+    
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
         
-        addSubview(button)
+        for (index, button) in timeboxButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (44 + timeboxSpacing))
+            button.frame = buttonFrame
+        }
     }
     
     override func intrinsicContentSize() -> CGSize {
         return CGSize(width: 240, height: 44)
+    }
+    
+    
+    // MARK: Button Action
+    
+    func timeboxButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
     }
 }
