f092b1472465d64ca71f066afc8efe2356be971f
Adland Lee <adland@4mfd.com>
2016-06-30 16:04:29 -0700

    save activity detail scroll layout

--- So Much So Little/ActivityDetailViewController.swift
+++ So Much So Little/ActivityDetailViewController.swift
@@ -12,22 +12,20 @@ class ActivityDetailViewController: UIViewController {
     
     var activity: Activity?
     
-//    
-//    @IBOutlet weak var taskTextField: UITextField!
-//    @IBOutlet weak var timeboxControl: TimeboxControl!
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var timeboxControl: TimeboxControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if let activity = activity {
-//            taskTextField.text = activity.task
-//            
-//            let actualTimeboxes = activity.actual_timeboxes!
-//            let estimatedTimeboxes = activity.estimated_timeboxes!
-//            
-//            timeboxControl.pendingTimeboxes = estimatedTimeboxes as Int
-//            timeboxControl.completedTimeboxes = actualTimeboxes as Int
-            print(activity)
+            taskTextField.text = activity.task
+            
+            let actualTimeboxes = activity.actual_timeboxes!
+            let estimatedTimeboxes = activity.estimated_timeboxes!
+            
+            timeboxControl.pendingTimeboxes = estimatedTimeboxes as Int
+            timeboxControl.completedTimeboxes = actualTimeboxes as Int
         }
     }
 }
