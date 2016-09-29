98b14f195dfd35ab316ac20fce81f749dcc1806f
Adland Lee <adland@4mfd.com>
2016-09-28 21:10:08 -0700

    add start/interrupt button

--- So Much So Little/TimerViewController.swift
+++ So Much So Little/TimerViewController.swift
@@ -10,13 +10,40 @@ import UIKit
 
 class TimerViewController: UIViewController {
     
+    @IBOutlet weak var startInterruptButton: UIButton!
+    
+    // MARK: - View Life Cycle
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowActivityTab" {
             print("ShowActivityTab")
-            let activityTabIndex = segue.destination.childViewControllers.index(where: {$0.title == "Activity Nav"})!
             let tabBar = segue.destination as! UITabBarController
+            let activityTabIndex = tabBar.childViewControllers.index(where: {$0.title == "Activity Nav"})!
             tabBar.selectedIndex = activityTabIndex
         }
     }
+    
+
+    // MARK: - Actions
+    
+    @IBAction func startActivityTimer(_ sender: AnyObject) {
+        print("startActivityTimer")
+        UIView.performWithoutAnimation {
+            sender.setTitle("Interrupt", for: .normal)
+            sender.removeTarget(self, action: #selector(startActivityTimer(_:)), for: .touchUpInside)
+            sender.addTarget(self, action: #selector(interruptActivityTimer(_:)), for: .touchUpInside)
+        }
+    }
+    
+    @IBAction func interruptActivityTimer(_ sender: AnyObject) {
+        print("interruptActivityTimer")
+        UIView.performWithoutAnimation {
+            sender.setTitle("Start", for: .normal)
+            sender.removeTarget(self, action: #selector(interruptActivityTimer(_:)), for: .touchUpInside)
+            sender.addTarget(self, action: #selector(startActivityTimer(_:)), for: .touchUpInside)
+        }
+    }
+    
+    // MARK: - Helper Functions
+    
 }
