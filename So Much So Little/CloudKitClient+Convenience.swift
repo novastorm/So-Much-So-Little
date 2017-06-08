0046d8911ddf83e23607db85702b193dfec7e210
Adland Lee <adland.lee@gmail.com>
2017-06-07 17:17:01 -0700

    clean up Cloud kit modification code

--- So Much So Little/CloudKitClient+Convenience.swift
+++ So Much So Little/CloudKitClient+Convenience.swift
@@ -12,41 +12,14 @@ import Foundation
 
 extension CloudKitClient {
     
-//    static func storeRecords(_ records: [CKRecord], using database: CKDatabase = privateDatabase, completionHandler: @escaping(_ record: CKRecord?, _ error: Error?) -> Void) {
-//        
-//        let modifyRecordsOperation = CKModifyRecordsOperation()
-//        modifyRecordsOperation.database = database
-//        modifyRecordsOperation.recordsToSave = records
-//        
-//        modifyRecordsOperation.perRecordCompletionBlock = { (ckRecord, error) in
-//            guard error == nil else {
-//                print("perRecordCompletionBlock", error!)
-//                return
-//            }
-//            print("Store record")
-//        }
-//        modifyRecordsOperation.modifyRecordsCompletionBlock = { (savedRecordList, deletedRecordIDList, error ) in
-//            guard error == nil else {
-//                print("modifyRecordsCompletionBlock", error!)
-//                completionHandler(nil, error)
-//                return
-//            }
-//            completionHandler(savedRecordList?.first!, error)
-//        }
-//        modifyRecordsOperation.start()
-//    }
-    
     static func storeRecords(using database: CKDatabase = privateDatabase, context: NSManagedObjectContext, completionHandler: @escaping(_ success: Bool, _ error: Error?) -> Void) {
         
-//        let insertedObjects = context.insertedObjects
-//        let updatedObjects = context.updatedObjects
         let modifiedObjects = context.insertedObjects.union(context.updatedObjects)
         let deletedObjects = context.deletedObjects
         
         let modifyRecordsOperation = CKModifyRecordsOperation()
         modifyRecordsOperation.database = database
         
-        // FIXME: join the two sets for ckloudkit processing
         modifyRecordsOperation.recordsToSave = modifiedObjects.flatMap { (record) -> CKRecord? in
             guard let record = record as? CloudKitManagedObject else {
                 return nil
@@ -55,14 +28,18 @@ extension CloudKitClient {
             return record.cloudKitRecord
         }
         
-        // FIXME: map ckrecordId for clokudkit processing
         modifyRecordsOperation.recordIDsToDelete = deletedObjects.flatMap { (record) -> CKRecordID? in
-            return nil
+            guard let record = record as? CloudKitManagedObject else {
+                return nil
+            }
+            
+            return CKRecord.decodeCKRecordSystemFields(from: record.encodedCKRecord!).recordID
         }
         
         modifyRecordsOperation.perRecordCompletionBlock = { (ckRecord, error) in
             guard error == nil else {
                 print("perRecordCompletionBlock", error!)
+                completionHandler(false, error)
                 return
             }
             
@@ -85,6 +62,7 @@ extension CloudKitClient {
         modifyRecordsOperation.modifyRecordsCompletionBlock = { (savedRecordList, deletedRecordIDList, error ) in
             guard error == nil else {
                 print("modifyRecordsCompletionBlock", error!)
+                completionHandler(false, error)
                 return
             }
             print("Store records complete")
